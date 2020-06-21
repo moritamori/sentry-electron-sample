7f131da18f939bf6460a24cd85abe0185c8367b2
moritamori <tamo@sonicgarden.jp>
2020-06-21 15:02:49 +0900

    メインプロセスのエラーを捕捉できるように

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const { app, BrowserWindow } = require("electron")
+const Sentry = require('@sentry/electron');
+
+Sentry.init({dsn: 'https://0d888243b2004ce5b89b5815b6d44169@o410344.ingest.sentry.io/5284198'});
 
 let win;
 function createWindow() {
   win = new BrowserWindow({ width: 800, height: 600})
+  myUndefinedFunction();
   win.loadURL(`file://${__dirname}/index.html`)
   win.on("closed", () => { win = null; })
 }
